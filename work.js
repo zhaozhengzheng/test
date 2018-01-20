0cb40ac0a4064fd30b73c3a845fc0562e5642b4d
zhaozhengzheng <zxmllz10@163.com>
2018-01-20 17:20:25 +0800

    zhaoyimeng

--- work.js
+++ work.js
@@ -1,41 +1,41 @@
 Vue.config.productionTip = true
-var one = {
+var Home = {
 	template: `
 		<div>
-			<div style="width: 400px;height: 500px;background-color: brown;position: absolute;top: 60px;left: 270px;">????</div>
-			<div style="width: 400px;height: 500px;background-color: blue;position: absolute;top: 60px;left: 690px;">????</div>
+			<div style="width: 400px;height: 500px;background-color: brown;position: absolute;top: 60px;left: 270px;">文章列表</div>
+			<div style="width: 400px;height: 500px;background-color: blue;position: absolute;top: 60px;left: 690px;">视频列表</div>
 		</div>
 `
 }
-var two = {
+var Event = {
 	template: `
 		<div style="width: 1000px;margin:auto;">
-			<div style="width: 1000px;height: 60px;background-color: dodgerblue;position: absolute;top: 100px;text-align:center;line-height:60px">????</div>
-			<div style="width: 1000px;height: 60px;background-color: dodgerblue;position: absolute;top: 200px;text-align:center;line-height:60px">????</div>
-			<div style="width: 1000px;height: 60px;background-color: dodgerblue;position: absolute;top: 300px;text-align:center;line-height:60px">????</div>
+			<div style="width: 1000px;height: 60px;background-color: dodgerblue;position: absolute;top: 100px;text-align:center;line-height:60px">文章列表</div>
+			<div style="width: 1000px;height: 60px;background-color: dodgerblue;position: absolute;top: 200px;text-align:center;line-height:60px">视频列表</div>
+			<div style="width: 1000px;height: 60px;background-color: dodgerblue;position: absolute;top: 300px;text-align:center;line-height:60px">视频列表</div>
 		</div>
 `
 }
-var three = {
+var Video = {
 	template: `
 		<div style="width: 1000px;margin:auto;">
-			<div style="width: 1000px;height: 60px;background-color: dodgerblue;position: absolute;top: 100px;text-align:center;line-height:60px">???</div>
-			<div style="width: 1000px;height: 500px;background-color: dodgerblue;position: absolute;top: 200px;text-align:center;line-height:60px">????</div>
+			<div style="width: 1000px;height: 60px;background-color: dodgerblue;position: absolute;top: 100px;text-align:center;line-height:60px">广告栏</div>
+			<div style="width: 1000px;height: 500px;background-color: dodgerblue;position: absolute;top: 200px;text-align:center;line-height:60px">视频内容</div>
 		</div>
 `
 }
 
 var routes = [{
 		path: "/",
-		component: one,
+		component: Home,
 },
 	{
 		path: "/events",
-		component: two,
+		component: Event,
 },
 	{
 		path: "/videos",
-		component: three,
+		component: Video,
 }]
 
 var router = new VueRouter({
